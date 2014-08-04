0093ead2783fc1ffe49b3d7a62f5fcf9b3b61c55
jdmcpeek <david.mcpeek@yale.edu>
2014-08-03 23:42:54 -0400

    finally migrated the devise file with all new user columns

--- db/migrate/20140723015944_add_name_to_users.rb
+++ db/migrate/20140723015944_add_name_to_users.rb
@@ -1,7 +1,7 @@
 class AddNameToUsers < ActiveRecord::Migration
   def change
     add_column :users, :name, :string
-    remove_column :users, :netid
+    
    
 
   end
--- db/migrate/20140723151459_add_netid_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddNetidToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :netid, :string
-  end
-end
